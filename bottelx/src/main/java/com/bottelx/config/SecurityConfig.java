003b06bd314c79f6a2cfce15c492d68b2ad70a07
satya gopal <satyamohanty918@gmail.com>
2026-06-05 20:23:27 +0530

    Updated CORS

--- bottelx/src/main/java/com/bottelx/config/SecurityConfig.java
+++ bottelx/src/main/java/com/bottelx/config/SecurityConfig.java
@@ -100,6 +100,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:8081",
                 "https://bottlex-1.onrender.com",
+                "https://bottlex-d7bzxg28p-satya918s-projects.vercel.app",
                 "http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));

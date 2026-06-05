7925c2bca2db8ac003b8331f7d7a5c3ef1e9c2c2
satya gopal <satyamohanty918@gmail.com>
2026-06-05 19:31:31 +0530

    Updated api endpoint

--- bottleX_manufactureres/bottlex-manufacturer-admin/src/api/apiClient.ts
+++ bottleX_manufactureres/bottlex-manufacturer-admin/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://bottlex-1.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
